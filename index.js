1b65c1a3ce415b3a3a69c8e88696453c0b7f58da
Pavel Korotenko <pavel.korotenko@rci.rogers.ca>
2019-11-01 16:40:10 -0400

    Step 2 part 1: new method for API.

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/', function (req, res) {
+  if (req.query && req.query.content)
+    res.send(req.query.content)
+  else
+    res.send('Use ?content parameter for this endpoint.')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
