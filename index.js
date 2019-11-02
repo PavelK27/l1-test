07a856e3c615a474cb3ac1a72f9724d33db646fa
Pavel Korotenko <pavel.korotenko@rci.rogers.ca>
2019-11-02 13:51:06 -0400

    Step 4: add database.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const formidable = require('express-formidable')
+
+const mh = require('mongodb-tiny-helper')(
+    'mongodb+srv://'+
+    process.env.DB_USER+':'+
+    process.env.DB_PASS+
+    '@cluster0-yn8qf.mongodb.net/test?retryWrites=true&w=majority',
+    'main'
+)
+
 const app = express()
 const port = 3000
 
@@ -7,11 +16,29 @@ app.use('/static', express.static('public'))
 app.use(formidable())
 app.get('/', (req, res) => res.send('Hello World!'))
 
+const sanitizeString = (str) => {
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+    return str.trim();
+}
+
 app.post('/', function (req, res) {
-  if (req.fields && req.fields.content)
-    res.send(req.fields.content)
-  else
-    res.send('Missing content value!')
+  if (req.fields && req.fields.content) {
+        req.fields.content = sanitizeString(req.fields.content)
+
+        let data = {
+            text: req.fields.content,
+            date: Date.now()
+        }
+
+        mh.insertOne('messages', data, function(val) {
+            if (val.insertedCount === 1)
+                res.send(req.fields.content)
+            else
+                res.send('error!')
+        })
+    } else {
+        res.send('Missing content value!')
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
